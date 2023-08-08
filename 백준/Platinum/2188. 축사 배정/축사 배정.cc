61d344d2228754052bb7aa27d01b5987c2c1e5bd
lolcern <105647545+lolcern@users.noreply.github.com>
2023-08-08 21:05:32 +0900

    [Platinum IV] Title: 축사 배정, Time: 0 ms, Memory: 2160 KB -BaekjoonHub

---
+++ 백준/Platinum/2188. 축사 배정/축사 배정.cc
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+#define inp ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(NULL);
+#define endl "\n"
+#define MOD 1000000007
+using namespace std;
+typedef pair<int, int> p;
+typedef long long int ll;
+
+vector<int> line[203];
+p g[203];
+int a[203], b[203];
+bool visited[203];
+int ans;
+
+bool dfs(int x) {
+	visited[x] = true;
+	for (auto y : line[x]) {
+		if (!b[y] || !visited[b[y]] && dfs(b[y])) {
+			a[x] = y;
+			b[y] = x;
+			return true;
+		}
+	}
+	return false;
+}
+
+int main() {
+	inp;
+	int n, m;
+	cin >> n >> m;
+	for (int i = 1; i <= n; i++) {
+		int a, b;
+		cin >> a;
+		while (a--) {
+			cin >> b;
+			line[i].push_back(b);
+		}
+	}
+
+	for (int i = 1; i <= n; i++) {
+		if (!a[i]) {
+			memset(visited, false, sizeof(visited));
+			if (dfs(i))ans++;
+		}
+	}
+	cout << ans;
+}
